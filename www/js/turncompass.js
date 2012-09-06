cd92eabaaa099a5782d5835f05579dcc649f0adc
mpberk <matthew.p.berk@intel.com>
2012-09-06 11:00:37 -0700

    Reorganized code

---
+++ www/js/turncompass.js
@@ -0,0 +1,30 @@
+﻿var curr_compass_pos;
+var compass_threshhold ;
+var first_time ;
+var stable_compass_pos; 
+
+function initcompass() {
+    curr_compass_pos = SensorState.compass.magNorth;
+    stable_compass_pos= curr_compass_pos;
+    compass_threshhold = 5;
+    first_time = 0;
+}
+
+function turnfromcompass() {
+
+    
+    curr_compass_pos = SensorState.compass.magNorth;
+
+    if (first_time == 0 ) {
+        stable_compass_pos = curr_compass_pos;
+        first_time = 1;
+    }
+
+    if (!((curr_compass_pos != stable_compass_pos) && (curr_compass_pos <= stable_compass_pos + compass_threshhold) && (curr_compass_pos >= compass_pos - compass_threshhold))){
+        stable_compass_pos = curr_compass_pos;
+       }
+  //  AddTapMessageRow("Current Compass Position" + stable_compass_pos);
+    }
+
+
+ 
